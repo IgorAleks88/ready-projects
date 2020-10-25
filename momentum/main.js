250653ca0fd16779aa594fa412901c7726bf7f8b
IgorAleks88 <igor.alexeyenko@gmail.com>
2020-10-25 12:26:54 +0200

    refactory: add changing of images and quotes

--- momentum/main.js
+++ momentum/main.js
@@ -2,12 +2,32 @@
 const date=document.querySelector('.date'),
     time=document.querySelector('.time'),
     greeting=document.querySelector('.greeting'),
-     name=document.querySelector('.name'),
+    name=document.querySelector('.name'),
     focus = document.querySelector('.focus'),
     weatherIcon = document.querySelector('.weather-icon'),
     temperature = document.querySelector('.temperature'),
     weatherDescription = document.querySelector('.weather-description'),
-    city = document.querySelector('.city');
+    focusDescription = document.querySelector('.focus-description'),
+    city = document.querySelector('.city'),
+    blockquote = document.querySelector('blockquote'),
+    figcaption = document.querySelector('figcaption'),
+    quoteBtn = document.querySelector('#quote-btn'),
+    imageBtn = document.querySelector('#image-btn'),
+    imageBase="url('./assets/images/",
+    images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'],
+    partsOfDay=['night/', 'morning/', 'day/', 'evening/'],
+    twentyFourHours=[];
+    let imageCount=0;
+
+//Make order of images for 24hours
+function imagesOrder(){
+for (let i=0; i<partsOfDay.length; i++){
+    for (let j=0; j<6; j++){
+        twentyFourHours.push(`${imageBase + partsOfDay[i] + images[Math.floor(Math.random() * images.length)]}')`);
+    }
+}
+}
+
 
 //Weather
 async function getWeather() {
@@ -124,7 +144,9 @@ function dayToString(num){
      hours=today.getHours(),
      mins=today.getMinutes(),
      secs=today.getSeconds();
-
+if (mins==0 &&secs==0){
+    setBackground(hours);
+}
     time.innerHTML=`${hours}<span>:</span>${addZero(mins)}<span>:</span>${addZero(secs)}`
     setTimeout(showTime, 1000);
  }
@@ -138,19 +160,25 @@ function dayToString(num){
      hours=today.getHours();
      if(hours<6) {
         greeting.textContent = 'Good Night, ';
-        document.body.style.backgroundImage="url('./assets/images/night/01.jpg')";
+        
      } else if (hours<12){
         greeting.textContent = 'Good Morning, ';
-        document.body.style.backgroundImage="url('./assets/images/morning/01.jpg')";
+       
      } 
      else if (hours<18){
          greeting.textContent = 'Good Afternoon, ';
-         document.body.style.backgroundImage="url('./assets/images/day/01.jpg')";
+         
          } else {
         greeting.textContent = 'Good Evening, ';
-        document.body.style.backgroundImage="url('./assets/images/evening/01.jpg')";
         }
+        setBackground(hours);  
     
+ }
+
+ //Set Background
+
+ function setBackground(n){
+    document.body.style.backgroundImage=twentyFourHours[n];
  }
 
  //Get name
@@ -164,15 +192,37 @@ function getName(){
 
 //Set name
 function setName(event){
+
 if (event.type==='keypress'){
     if (event.which==13 || event.keyCode==13){
+        if (event.target.innerText=='') {
+            event.target.innerText='[Enter name]';
+        } else {
         localStorage.setItem('name', event.target.innerText); 
         name.blur();
     }
+}
 } 
 else {
+    if (event.target.innerText=='') {
+        event.target.innerText='[Enter name]';
+    } else {
     localStorage.setItem('name', event.target.innerText);
 }
+}
+}
+
+//Clear Name
+function clearName() {
+    localStorage.removeItem('name'); 
+    name.textContent='[Enter name]';
+}
+
+//Input Name
+function inputName(){
+    if (localStorage.getItem('name')===null) {
+        name.textContent=''; 
+    }
 }
 
  //Get focus
@@ -186,23 +236,80 @@ else {
 
 //Set focus
 function setFocus(event){
+   
     if (event.type==='keypress'){
         if (event.which==13 || event.keyCode==13){
+            if (event.target.innerText=='') {
+                event.target.innerText='[Enter name]';
+            } else {
             localStorage.setItem('focus', event.target.innerText); 
             focus.blur();
-        }
+            }
+        } 
     } 
     else {
+        if (event.target.innerText=='') {
+            event.target.innerText='[Enter name]';
+        } else {
         localStorage.setItem('focus', event.target.innerText);
     }
-    }
+}
+    
+}
 
+//Clear Focus
+function clearFocus() {
+    localStorage.removeItem('focus'); 
+    focus.textContent='[Enter name]';
+}
+
+//Input Focus
+function inputFocus(){
+    if (localStorage.getItem('focus')===null) {
+        focus.textContent=''; 
+    }
+}
+
+
+//Quote Changing
+async function getQuote() {  
+    const url = `https://type.fit/api/quotes`;
+    const res = await fetch(url);
+    const data = await res.json(); 
+    let qn=Math.floor(Math.random() * data.length);
+    blockquote.textContent = data[qn-1].text;
+    figcaption.textContent = data[qn-1].author;
+  }
+
+  //Image Changing
+
+function getImage(){
+    imageCount++;
+    setBackground(imageCount);
+    imageCount++;
+    if (imageCount==24){
+        imageCount=0;
+    }
+    imageBtn.disabled = true;
+    setTimeout(function() { imageBtn.disabled = false }, 1000);
+    
+
+}
+
+name.addEventListener('click', inputName);
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
+greeting.addEventListener('click', clearName);
+focusDescription.addEventListener('click', clearFocus);
 focus.addEventListener('keypress', setFocus);
+focus.addEventListener('click', inputFocus);
 focus.addEventListener('blur', setFocus);
+document.addEventListener('DOMContentLoaded', getQuote);
+quoteBtn.addEventListener('click', getQuote);
+imageBtn.addEventListener('click', getImage);
 
  //Run
+ imagesOrder();
  showDate();
  showTime();
  setBackgroundAndGreet();
